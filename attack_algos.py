5b90f8005d3d9c74bf21a556d08dbd005239a810
Ben Pinhasov <ben.pinhasov94@gmail.com>
2024-04-04 14:27:38 +0300

    start implement adaptive black box in batches

--- attack_algos.py
+++ attack_algos.py
@@ -579,7 +579,7 @@ def adaptive_black_box_attack(input_img, deepfake_detector_model, deepfake_detec
         g = 0
         _num_queries = 0
         for sample_no in range(num_samples):
-            #for transform_func in transform_functions:
+            # for transform_func in transform_functions:
             rand_noise = torch.randn_like(input_var)
             img1 = input_var + sigma * rand_noise
             img2 = input_var - sigma * rand_noise
@@ -699,7 +699,204 @@ def adaptive_black_box_attack(input_img, deepfake_detector_model, deepfake_detec
         'l_inf_norm': l_inf_norm,
         'l_inf_norm_255': round(l_inf_norm * 255.0)
     }
+    return input_var, meta_data
 
+
+def adaptive_black_box_attack_batches(input_img, deepfake_detector_model, deepfake_detector_model_type,
+                                      attacked_detector_model, xai_calculator, xai_method, crop_size,
+                                      cuda=True, max_iter=100, alpha=1 / 255.0,
+                                      eps=16 / 255.0, desired_acc=0.90,
+                                      transform_set={"gauss_blur", "translation"}):
+    def _get_transforms(apply_transforms={"gauss_noise", "gauss_blur", "translation", "resize"}):
+
+        transform_list = [
+            lambda x: x,
+        ]
+
+        if "gauss_noise" in apply_transforms:
+            transform_list += [
+                lambda x: rt.add_gaussian_noise(x, 0.01, cuda=cuda),
+            ]
+
+        if "gauss_blur" in apply_transforms:
+            kernel_size = random.randint(3, 6)
+            if kernel_size % 2 == 0:
+                kernel_size = kernel_size - 1
+            sigma = random.randint(5, 7)
+            transform_list += [
+                lambda x: rt.gaussian_blur(x, kernel_size=(kernel_size, kernel_size), sigma=(sigma * 1., sigma * 1.),
+                                           cuda=cuda)
+            ]
+
+        if "translation" in apply_transforms:
+            x_translate = random.randint(-20, 20)
+            y_translate = random.randint(-20, 20)
+
+            transform_list += [
+                lambda x: rt.translate_image(x, x_translate, y_translate, cuda=cuda),
+            ]
+
+        if "resize" in apply_transforms:
+            compression_factor = random.randint(4, 6) / 10.0
+            transform_list += [
+                lambda x: rt.compress_decompress(x, compression_factor, cuda=cuda),
+            ]
+
+        return transform_list
+
+    def _find_nes_gradient(input_var, transform_functions, deepfake_detector_model, deepfake_detector_model_type,
+                           attacked_detector_model, xai_calculator, crop_size, xai_method, num_samples=10, sigma=0.001):
+        g = 0
+        _num_queries = 0
+        input_shape = input_var.shape[1:]
+        rand_noise = torch.randn(num_samples, num_samples, *input_shape, device=input_var.device) * sigma
+
+        imgs1 = input_var.expand(num_samples, -1, -1, -1) + sigma * rand_noise
+        imgs2 = input_var.expand(num_samples, -1, -1, -1) - sigma * rand_noise
+
+        unprocessed_imgs1 = un_preprocess_image_batch(imgs1, crop_size)
+        unprocessed_imgs2 = un_preprocess_image_batch(imgs2, crop_size)
+
+        xais1 = calculate_xai_map_batch(unprocessed_imgs1, deepfake_detector_model, deepfake_detector_model_type,
+                                        xai_calculator, xai_method, cuda=cuda)
+        xais2 = calculate_xai_map_batch(unprocessed_imgs2, deepfake_detector_model, deepfake_detector_model_type,
+                                        xai_calculator, xai_method, cuda=cuda)
+
+        _, attacked_probs1, _ = check_attacked_batch(img1, xais1, deepfake_detector_model, cuda=cuda)
+        _, attacked_probs2, _ = check_attacked_batch(img2, xais2, deepfake_detector_model, cuda=cuda)
+
+        predictions1, probs1 = predict_with_model_batch(imgs1, deepfake_detector_model, deepfake_detector_model_type,
+                                                        cuda=cuda)
+        prediction2, probs2 = predict_with_model_batch(img2, deepfake_detector_model, deepfake_detector_model_type,
+                                                       cuda=cuda)
+        _num_queries += 2 * num_samples
+
+        g = ((probs_1[:, :, 0] + attacked_probs1[:, :, 0]) - (
+                    probs_2[:, :, 0] + attacked_probs2[:, :, 0])) @ rand_noise.view(batch_size * num_samples, -1)
+        g = g.sum(dim=0).data.detach()
+        #TODO: Understand what is dims of g one the batch_Size is 1
+
+        for sample_no in range(num_samples):
+            # for transform_func in transform_functions:
+            rand_noise = torch.randn_like(input_var)
+            img1 = input_var + sigma * rand_noise
+            img2 = input_var - sigma * rand_noise
+
+            unprocessed_img1 = un_preprocess_image(img1, crop_size)
+            unprocessed_img2 = un_preprocess_image(img2, crop_size)
+            xai1 = calculate_xai_map(unprocessed_img1, deepfake_detector_model, deepfake_detector_model_type,
+                                     xai_calculator, xai_method, cuda=cuda)
+            xai2 = calculate_xai_map(unprocessed_img2, deepfake_detector_model, deepfake_detector_model_type,
+                                     xai_calculator, xai_method, cuda=cuda)
+            with torch.no_grad():
+                _, attacked_probs1, _ = check_attacked((img1), xai1, attacked_detector_model,
+                                                       cuda=cuda)
+                _, attacked_probs2, _ = check_attacked((img2), xai2, attacked_detector_model,
+                                                       cuda=cuda)
+                prediction1, probs_1, _ = predict_with_model((img1), deepfake_detector_model,
+                                                             deepfake_detector_model_type, cuda=cuda)
+
+                prediction2, probs_2, _ = predict_with_model((img2), deepfake_detector_model,
+                                                             deepfake_detector_model_type, cuda=cuda)
+
+            _num_queries += 2
+            g = g + (probs_1[0][0] + attacked_probs1[0][0]) * rand_noise
+            g = g - (probs_2[0][0] + attacked_probs2[0][0]) * rand_noise
+            g = g.data.detach()
+
+            del rand_noise
+            del img1
+            del prediction1, probs_1
+            del prediction2, probs_2
+            del attacked_probs1
+            del attacked_probs2
+
+        return (1. / (2. * num_samples * len(transform_functions) * sigma)) * g, _num_queries
+
+    input_var = autograd.Variable(input_img, requires_grad=True)
+
+    target_var = autograd.Variable(torch.LongTensor([0]))
+    if cuda:
+        target_var = target_var.cuda()
+
+    iter_no = 0
+
+    # give it a warm start by crafting by fooling without any transformations -> easier
+    warm_start_done = False
+    num_queries = 0
+    while iter_no < max_iter:
+
+        if not warm_start_done:
+            with torch.no_grad():
+                _, output, _ = predict_with_model(input_var, deepfake_detector_model, deepfake_detector_model_type,
+                                                  cuda=cuda)
+            unprocessed_img = un_preprocess_image(input_var, crop_size)
+            xai = calculate_xai_map(unprocessed_img, deepfake_detector_model, deepfake_detector_model_type,
+                                    xai_calculator, xai_method, cuda=cuda)
+            with torch.no_grad():
+                _, attacked_probs, _ = check_attacked(input_var, xai, attacked_detector_model, cuda=cuda)
+            num_queries += 1
+            if (output[0][0] > desired_acc) and (attacked_probs[0][0] > 0.9):
+                warm_start_done = True
+
+        if warm_start_done:
+            # choose all transform functions
+            transform_functions = _get_transforms(transform_set)
+        else:
+            transform_functions = _get_transforms({})  # returns identity function
+
+        all_fooled = True
+        print("Testing transformation outputs", iter_no)
+        for transform_fn in transform_functions:
+            with torch.no_grad():
+                _, output, _ = predict_with_model(transform_fn(input_var), deepfake_detector_model,
+                                                  deepfake_detector_model_type, cuda=cuda)
+            unprocessed_img = un_preprocess_image(input_var, crop_size)
+            xai = calculate_xai_map(unprocessed_img, deepfake_detector_model, deepfake_detector_model_type,
+                                    xai_calculator, xai_method, cuda=cuda)
+            with torch.no_grad():
+                _, attacked_probs, _ = check_attacked(transform_fn(input_var), xai, attacked_detector_model, cuda=cuda)
+            num_queries += 1
+            print(output)
+            print(attacked_probs)
+            if (output[0][0] < desired_acc) or (attacked_probs[0][0] < 0.9):
+                all_fooled = False
+
+        print("All transforms fooled:", all_fooled, "Warm start done:", warm_start_done)
+        if warm_start_done and all_fooled:
+            break
+
+        t_start = time.time()
+        step_gradient_estimate, _num_grad_calc_queries = _find_nes_gradient(input_var=input_var,
+                                                                            transform_functions=transform_functions,
+                                                                            deepfake_detector_model=deepfake_detector_model,
+                                                                            deepfake_detector_model_type=deepfake_detector_model_type,
+                                                                            attacked_detector_model=attacked_detector_model,
+                                                                            xai_calculator=xai_calculator,
+                                                                            crop_size=crop_size,
+                                                                            xai_method=xai_method)
+        print(f'Gradient calculation time: {time.time() - t_start}')
+        num_queries += _num_grad_calc_queries
+        step_adv = input_var.detach() + alpha * torch.sign(step_gradient_estimate.data.detach())
+        total_pert = step_adv - input_img
+        total_pert = torch.clamp(total_pert, -eps, eps)
+
+        input_adv = input_img + total_pert
+        input_adv = torch.clamp(input_adv, 0, 1)
+
+        input_var.data = input_adv.detach()
+
+        iter_no += 1
+
+    l_inf_norm = torch.max(torch.abs((input_var - input_img))).item()
+    print("L infinity norm", l_inf_norm, l_inf_norm * 255.0)
+
+    meta_data = {
+        'num_network_queries': num_queries,
+        'attack_iterations': iter_no,
+        'l_inf_norm': l_inf_norm,
+        'l_inf_norm_255': round(l_inf_norm * 255.0)
+    }
     return input_var, meta_data
 
 
